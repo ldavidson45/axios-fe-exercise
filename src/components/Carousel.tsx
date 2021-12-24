a6a12905f006d22b87cfd4e57238abc291271f22
Liz Davidson <ldavidson@politico.com>
2021-12-24 17:23:23 -0600

    Refactor logic to navigate carousel; add arrow buttons, header, and additional styling

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,12 +5,14 @@ import styled from 'styled-components'
 import CarouselItem from './CarouselItem.tsx'
 import { CarouselProps } from '../utils/carouselTypes'
 
+
 export const Carousel = (props: CarouselProps) => {
 	const handleMediaQueryChange = isMobile => {
 		setCarouselSpecs({
-			countVisibleItems: isMobile ? 1 : 3,
-			isMobile,
-			activeGroupIndex: 0
+            ...carouselSpecs,
+			countVisibleItems: isMobile ? 2 : 3,
+			activeGroupIndex: 0,
+
 		})
 	}
 	const isMobile = useMediaQuery(
@@ -19,45 +21,53 @@ export const Carousel = (props: CarouselProps) => {
 		handleMediaQueryChange
 	)
 	const [carouselSpecs, setCarouselSpecs] = useState({
-		countVisibleItems: isMobile ? 1 : 3,
-		isMobile,
-		activeGroupIndex: 0
+		countVisibleItems: isMobile ? 2 : 3,
+		activeGroupIndex: 0,
+        activeItemIndex: 0,
 	})
 
-	const { countVisibleItems, activeGroupIndex } = carouselSpecs
+    const { countVisibleItems, activeGroupIndex, activeItemIndex } = carouselSpecs
 
-    const itemGroups = () => {
-        const allItems = [...props.articles]
-        let groups = []
-        while(allItems.length > 0) {
-            const group = allItems.splice(0, countVisibleItems)
-            groups.push(group)
-        }
-        return groups
+    const visibleItems = () => {
+        const articles = [...props.articles]
+        return articles.slice(activeItemIndex, activeItemIndex + countVisibleItems)
     }
-    const visibleItems = itemGroups()[activeGroupIndex]
 
+    const nextItemIndex = () => {
+        return isMobile ? activeItemIndex + 1 : activeItemIndex + countVisibleItems
+    }
 
-	const updateVisibleItems = direction => {
-        let newActiveGroup;
-        const lastGroupIndex = itemGroups().length - 1
-        const isLastGroup = activeGroupIndex === lastGroupIndex
-        const isFirstGroup = activeGroupIndex === 0
+    const previousItemIndex = () => {
+        return isMobile ? activeItemIndex - 1 : activeItemIndex - countVisibleItems
+    }
 
+    const isNavigationButtonDisabled = (direction) => {
         if (direction === 'next') {
-            newActiveGroup = isLastGroup ? 0 : activeGroupIndex + 1
-        } 
-        if (direction === 'previous') {
-            newActiveGroup = isFirstGroup ? lastGroupIndex : activeGroupIndex - 1
+            return !props.articles[nextItemIndex()]
         }
-       
+        if (direction === 'previous') {
+            return !props.articles[previousItemIndex()]
+        }
+
+
+    }
+
+    const updateActiveItem = (direction) => {
+        let newActiveItem;
+        if (direction === 'next') {
+            newActiveItem = nextItemIndex()
+        } else if (direction === 'previous') {
+            newActiveItem = previousItemIndex()
+        }
+        
         setCarouselSpecs({
             ...carouselSpecs,
-            activeGroupIndex: newActiveGroup
+            activeItemIndex: newActiveItem
         })
-	}
+    }
 
-	const itemsList = visibleItems.map((item, index) => {
+
+	const itemsList = visibleItems().map((item, index) => {
 		const {
 			id,
 			headline,
@@ -76,20 +86,105 @@ export const Carousel = (props: CarouselProps) => {
 			primary_image,
 			topics
 		}
-		return <CarouselItem {...articleData} key={index} />
+		return <div key={index}>
+            {props.articles.indexOf(item)}
+            <CarouselItem {...articleData}  />
+        </div>
 	})
 
 	return (
 		<CarouselContainer>
-			<button onClick={() => updateVisibleItems('previous')}>Previous</button>
-			{itemsList}
-			<button onClick={() => updateVisibleItems('next')}>next</button>
+			<NavigationArrow>
+				<button
+					onClick={() => updateActiveItem('previous')}
+					aria-label="previous"
+					type="button"
+                    disabled={isNavigationButtonDisabled('previous')}
+				/>
+			</NavigationArrow>
+			<CarouselContent>
+				<div className="header">
+					<h4 className="header__title">More From Axios.com</h4>
+					<a
+						className="header__link"
+						href="https://www.axios.com/"
+						target="_blank"
+                        rel="noreferrer"
+					>
+						Visit Axios.com
+					</a>
+				</div>
+				<div className="cards">
+                    {itemsList}
+                </div>
+			</CarouselContent>
+
+			<NavigationArrow isRight>
+				<button
+					onClick={() => updateActiveItem('next')}
+					aria-label="next"
+					type="button"
+                    disabled={isNavigationButtonDisabled('next')}
+				/>
+			</NavigationArrow>
 		</CarouselContainer>
 	)
 }
 
 const CarouselContainer = styled.div`
 	display: flex;
-	width: 100%;
+	width: 100vw;
 	padding: 40px 60px;
+	align-items: stretch;
+`
+
+const CarouselContent = styled.div`
+	overflow: hidden;
+	flex-grow: 3;
+
+	.header {
+		display: flex;
+		justify-content: space-between;
+
+		&__title {
+			font-size: 2.375rem;
+			font-weight: 300;
+			color: #333335;
+			padding-left: 50px;
+		}
+
+		&__link {
+			align-self: center;
+			color: #fff;
+			background: #2257da;
+			padding: 10px;
+			border-radius: 6px;
+			font-size: 18px;
+		}
+	}
+
+	.cards {
+		display: flex;
+	}
+`
+
+const NavigationArrow = styled.div`
+	display: flex;
+	align-items: center;
+	width: 60px;
+	button {
+		width: 40px;
+		height: 40px;
+		border: 3px solid #bababe;
+		border-top: none;
+		border-right: none;
+		transform: rotate(${props => (props.isRight ? '-135deg' : '45deg')});
+
+        :hover:enabled {
+            border-color: #2257DA
+        }
+        :disabled {
+            border: none;
+        }
+	}
 `
--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,12 +1,32 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
   body {
+    margin: 0;
+    padding: 0;
     font-family: Open-Sans, Helvetica, Sans-Serif;
   }
 
   a {
     text-decoration: none;
+  }
+
+  button {
+    background: none;
+    border: none;
+    padding: 0;
+
+    :hover {
+      cursor: pointer;
+    }
+
+    :disabled {
+      cursor: default;
+    }
+
   }
 `;
  
